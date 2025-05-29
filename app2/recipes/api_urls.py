69fe55299bc13305b3da9a12ae2d2bf6bb1ed449
DanDevaty <daniel.devaty@gmail.com>
2025-05-29 18:17:08 +0200

    Remade api

--- app2/recipes/api_urls.py
+++ app2/recipes/api_urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from .api_views import Recipes_view_set
 from rest_framework.routers import DefaultRouter
+from recipes import api_views
 
 router = DefaultRouter()
 router.register(r'recipes', Recipes_view_set)
